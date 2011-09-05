04e0ec19348f13de4ad105e94c74e73999db3165
drlrdsen <tuerk@flagbit.de>
2011-09-05 13:53:12 +0000

    [~BUGFIX] Added simple check for array key overflow
    git-svn-id: https://svn.dev.flagbit.com/svn/magento-extensions/FactFinder/trunk@886 5a9ba00c-e822-4c59-8af6-9966110bd143

--- src/app/code/community/Flagbit/FactFinder/Model/Adapter.php
+++ src/app/code/community/Flagbit/FactFinder/Model/Adapter.php
@@ -11,9 +11,9 @@ require_once BP.DS.'lib'.DS.'FACTFinder'.DS.'Loader.php';
 
 /**
  * Model class
- * 
+ *
  * Adapter between FACT-Finder API Framework and Magento
- * 
+ *
  * @category  Mage
  * @package   Flagbit_FactFinder
  * @copyright Copyright (c) 2010 Flagbit GmbH & Co. KG (http://www.flagbit.de/)
@@ -28,58 +28,58 @@ class Flagbit_FactFinder_Model_Adapter
 	 * @var FACTFinder_Abstract_SearchAdapter
 	 */
     protected $_searchAdapter = null;
-    
+
 	/**
 	 * FACT-Finder Suggestadapter
 	 * @var FACTFinder_Abstract_SuggestAdapter
 	 */
-    protected $_suggestAdapter = null;    
-    
+    protected $_suggestAdapter = null;
+
 	/**
 	 * FACT-Finder Config
 	 * @var FACTFinder_Abstract_Configuration
-	 */    
+	 */
     protected $_config = null;
-    
+
 	/**
 	 * FACT-Finder Parameter Parser
 	 * @var FACTFinder_ParametersParser
-	 */    
+	 */
     protected $_paramsParser = null;
-    
+
 	/**
 	 * FACT-Finder Data Provider
 	 * @var FACTFinder_Abstract_DataProvider
-	 */    
+	 */
     protected $_dataProvider = null;
-    
+
 	/**
 	 * FACT-Finder Scic Adapter
 	 * @var FACTFinder_Abstract_ScicAdapter
-	 */    
-    protected $_scicAdapter = null;    
-    
+	 */
+    protected $_scicAdapter = null;
+
 	/**
 	 * FACT-Finder After Search Navigation
 	 * @var array
-	 */    
+	 */
     protected $_afterSearchNavigation = null;
-    
+
 	/**
 	 * FACT-Finder Searchadapter
 	 * @var array
-	 */    
+	 */
     protected $_searchResultProductIds = null;
-    
+
     /**
-     * current FACT-Finder Category Path 
+     * current FACT-Finder Category Path
      * @var array
      */
     protected $_currentFactfinderCategoryPath = null;
 
     /**
      * get FactFinder SearchAdapter
-     * 
+     *
      * @return FACTFinder_Abstract_SearchAdapter
      */
     protected function _getSearchAdapter($collectParams = true)
@@ -89,37 +89,37 @@ class Flagbit_FactFinder_Model_Adapter
             $encodingHandler     = FF::getSingleton('encodingHandler', $config);
             $dataProvider        = $this->_getDataProvider();
             $this->_searchAdapter = FF::getSingleton(
-            	'xml65/searchAdapter', 
-            	$dataProvider, 
-            	$this->_getParamsParser(), 
+            	'xml65/searchAdapter',
+            	$dataProvider,
+            	$this->_getParamsParser(),
             	$encodingHandler
             );
-			
+
             if($collectParams == true){
             	$this->_collectParams();
-            }                    
+            }
         }
-        
+
         return $this->_searchAdapter;
     }
-    
+
     protected function _collectParams()
     {
         // search Helper
-        $helper = Mage::helper('factfinder/search');    	
+        $helper = Mage::helper('factfinder/search');
     	$_request = Mage::app()->getRequest();
     	$_query = $helper->getQuery()->getQueryText();
 
     	switch($_request->getModuleName()){
-    		
+
     		case "xmlconnect":
 		    	$this->_setParam('idsOnly', 'true')
 		    		->_setParam('productsPerPage', $_request->getParam('count'))
 		    		->_setParam('query', $helper->getQuery()->getQueryText())
-		    		->_setParam('page', ($_request->getParam('offset') / $_request->getParam('count')) + 1);   
-		    		
+		    		->_setParam('page', ($_request->getParam('offset') / $_request->getParam('count')) + 1);
+
 				// add Sorting Param
-				$params = Mage::app()->getRequest()->getParams();		    		
+				$params = Mage::app()->getRequest()->getParams();
 				foreach($params as $key => $value){
 					if(substr($key, 0, 6) == 'order_'){
 						$key = substr($key, 6);
@@ -127,12 +127,12 @@ class Flagbit_FactFinder_Model_Adapter
 							$this->_setParam('sort'.$key, $value);
 						}
 					}
-				}	
+				}
 
     			 // add Filter Params
 		    	$params = Mage::app()->getRequest()->getParams();
 		    	foreach($params as $key => $value){
-		    		$value = base64_decode($value);  		
+		    		$value = base64_decode($value);
 		    		if(strpos($value, '|')){
 		    			$param = explode('|', $value);
     					if($key == 'category'){
@@ -144,51 +144,51 @@ class Flagbit_FactFinder_Model_Adapter
     						}
     					}else{
     						$this->_setParam('filter'.$param[0], $param[1]);
-    					}	
+    					}
 		    		}
-		    	}		    		
-		    		
+		    	}
+
     			break;
-    			
+
     		case "catalog":
-                $_query = '*';	                  
+                $_query = '*';
                 Mage::app()->getRequest()->setParam('category', Mage::app()->getRequest()->getParam('category') ? Mage::app()->getRequest()->getParam('category') : $this->_getCurrentFactfinderCategoryPath());
-    		    
-    			
-    		case "catalogsearch": 
+
+
+    		case "catalogsearch":
     		default:
 		            // add Default Params
 		    	$this->_setParam('idsOnly', 'true')
 		    		->_setParam('productsPerPage', $helper->getPageLimit())
 		    		->_setParam('query', $_query)
 		    		->_setParam('page', $helper->getCurrentPage());
-		    		    		    
+
 		    	// add Sorting Param, but only if it was set explicit via url
-				$params = Mage::app()->getRequest()->getParams();		    		
+				$params = Mage::app()->getRequest()->getParams();
 				foreach($params as $key => $value){
 					if($key == 'order'
-					&& $helper->getCurrentOrder() 
+					&& $helper->getCurrentOrder()
 		    		&& $helper->getCurrentDirection()
 		    		&& $helper->getCurrentOrder() != 'position'
-		    		&& $helper->getCurrentOrder() != 'relevance'){		    		
+		    		&& $helper->getCurrentOrder() != 'relevance'){
 		    			$this->_setParam('sort'.$helper->getCurrentOrder(), $helper->getCurrentDirection());
 					}
 		    	}
-		
+
 		    	// add Filter Params
 		    	$params = Mage::app()->getRequest()->getParams();
-		    	foreach($params as $key => $value){  		
+		    	foreach($params as $key => $value){
 		    		if(strpos($value, '|')){
 		    			$param = explode('|', $value);
 		    			switch($param[1]){
-		    				
+
 		    				case 'slider':
 		    					$subparam = explode(':', $param[2]);
 		    					$this->_setParam($subparam[0], $subparam[1]);
 		    					$subparam = explode(':', $param[3]);
-		    					$this->_setParam($subparam[0], $subparam[1]);	    					
+		    					$this->_setParam($subparam[0], $subparam[1]);
 		    					break;
-		    					
+
 		    				default:
 		    					if($key == 'category'){
 		    						$categories = array_merge(array_slice(explode('/', $param[0]), 1), array($param[1]));
@@ -199,17 +199,17 @@ class Flagbit_FactFinder_Model_Adapter
 		    						}
 		    					}else{
 		    						$this->_setParam('filter'.$param[0], $param[1]);
-		    					}		
+		    					}
 		    					break;
 		    			}
 		    		}
-		    	}      			
+		    	}
     			break;
-    		
+
     	}
     }
-    
-    
+
+
     /**
      * execute search
      */
@@ -226,11 +226,11 @@ class Flagbit_FactFinder_Model_Adapter
             $status = false;
         }
         return $status;
-    }    
-    
+    }
+
     /**
      * get Redirect URL if there is set one
-     * 
+     *
      * @return string
      */
     public function getRedirect()
@@ -240,21 +240,21 @@ class Flagbit_FactFinder_Model_Adapter
 
 		if (!empty($campaigns) && $campaigns->hasRedirect()) {
 			$url = $campaigns->getRedirectUrl();
-		} 
-    	return $url;	
+		}
+    	return $url;
     }
-    
+
     /**
-     * 
+     *
      */
     public function getCampaigns()
     {
-		return $this->_getSearchAdapter()->getCampaigns();  	
+		return $this->_getSearchAdapter()->getCampaigns();
     }
-    
+
     /**
      * get Search Suggest URL
-     * 
+     *
      * @return string
      */
     public function getSuggestUrl()
@@ -262,13 +262,13 @@ class Flagbit_FactFinder_Model_Adapter
     	$dataprovider = $this->_getDataProvider();
     	$dataprovider->setType('Suggest.ff');
     	$dataprovider->setParams(array());
-    				
-    	return $dataprovider->getNonAuthenticationUrl(); 	
+
+    	return $dataprovider->getNonAuthenticationUrl();
     }
-    
+
     /**
      * get Suggest Adapter
-     * 
+     *
      * @return FACTFinder_Abstract_SuggestAdapter
      */
     protected function _getSuggestAdapter()
@@ -286,7 +286,7 @@ class Flagbit_FactFinder_Model_Adapter
 
     /**
      * get Suggest Results as Array
-     * 
+     *
      * @param string $query
      * @return array
      */
@@ -295,12 +295,12 @@ class Flagbit_FactFinder_Model_Adapter
 		$this->_setParam('query', $query);
 		$this->_setParam('format', 'json');
 
-		return Zend_Json_Decoder::decode($this->_getSuggestAdapter()->getSuggestions());		
+		return Zend_Json_Decoder::decode($this->_getSuggestAdapter()->getSuggestions());
     }
-    
+
     /**
      * get Suggest Results as JSON
-     * 
+     *
      * @param string $query
      * @return string json
      */
@@ -308,13 +308,13 @@ class Flagbit_FactFinder_Model_Adapter
     {
 		$this->_setParam('query', $query, false);
 		$this->_setParam('format', 'json', false);
-		
-		return $jqueryCallback.'('.$this->_getSuggestAdapter()->getSuggestions().')';		
-    }    
-    
+
+		return $jqueryCallback.'('.$this->_getSuggestAdapter()->getSuggestions().')';
+    }
+
     /**
      * get Scic Adapter
-     * 
+     *
      * @return FACTFinder_Abstract_ScicAdapter
      */
     public function getScicAdapter()
@@ -327,58 +327,58 @@ class Flagbit_FactFinder_Model_Adapter
             $this->_scicAdapter = FF::getSingleton('http/scicAdapter', $dataProvider, $this->_getParamsParser(), $encodingHandler);
         }
         return $this->_scicAdapter;
-    }    
-    
+    }
+
     /**
      * get Search Result Count
-     * 
+     *
      * @return int
      */
     public function getSearchResultCount()
     {
     	return $this->_getSearchAdapter()->getResult()->getFoundRecordsCount();
     }
-    
+
     /**
      * get After Search Navigation as Array
      * this simulates Magento Filter Attributes with Options
-     * 
+     *
      * @return array
      */
     public function getAfterSearchNavigation()
     {
     	if($this->_afterSearchNavigation == null){
-	    	$this->_afterSearchNavigation = array();    	
+	    	$this->_afterSearchNavigation = array();
 	    	$result = $this->_getSearchAdapter()->getAsn();
-	
-	        if ($result instanceof FACTFinder_Asn 
+
+	        if ($result instanceof FACTFinder_Asn
 	        	&& count($result)){
-	    		
+
 	        	foreach ($result as $row) {
 	        		$this->_afterSearchNavigation[] = array(
 	                	'attribute_code' => $row->getName(),
 	                	'name' => $row->getName(),
 	        			'unit' => $row->getUnit(),
 	                	'items' => $this->_getAttributeOptions($row->getArrayCopy()),
-	                	'count' => $row->count(), 
+	                	'count' => $row->count(),
 	        			'type'	=> $this->_getFilterType($row->getArrayCopy()),
-	                	'store_label' => $row->getName() 
+	                	'store_label' => $row->getName()
 	                );
-	        	}    		 
-	    	}	
+	        	}
+	    	}
     	}
 
     	return $this->_afterSearchNavigation;
     }
-    
+
     /**
      * get Filter Type by FACT-Finder FilterItem
-     * 
+     *
      * @param array $options
      * @return string
      */
     protected function _getFilterType($options)
-    {	
+    {
     	$defaultType = 'item';
     	foreach($options as $option){
     		if(!$option->getType()){
@@ -389,21 +389,21 @@ class Flagbit_FactFinder_Model_Adapter
     	}
     	return $defaultType;
     }
-    
+
     /**
      * get Attribute Options Array from FactFinder FilterGroupItems
-     * 
+     *
      * @param FACTFinder_AsnFilterItem $options
      * @return array
      */
     protected function _getAttributeOptions($options)
-    {	
+    {
     	$attributeOption = array();
     	$_currentCategoryPath = $this->_getCurrentFactfinderCategoryPath(true);
     	foreach($options as $option){
-    		
+
     		switch ($option->getType()){
-				
+
     			case "slider":
 					$attributeOption[] = array(
 						'type'	=> $option->getType(),
@@ -417,39 +417,39 @@ class Flagbit_FactFinder_Model_Adapter
 						'selected' => false //$option->isSelected()
 					);
     				break;
-    				
+
     			default:
-		    		if (!Mage::helper('core/string')->strlen($option->getValue())) {	
+		    		if (!Mage::helper('core/string')->strlen($option->getValue())) {
 		    			continue;
 		    		}
 		    		// remove Categories from top Level Navigation
 		    		$_value = $this->_getAttributeOptionValue($option);
 		    		if(Mage::getStoreConfigFlag('factfinder/config/navigation')
-		    		    && ( 
-		    		    empty($_value) === true 
+		    		    && (
+		    		    empty($_value) === true
 		    		    || in_array($_value, $this->_getCurrentFactfinderCategoryPath(true))
 		    		        && $_currentCategoryPath[count($_currentCategoryPath)-1] != $_value
 		    		    )){
 		    		        continue;
-		    		}      
-		    		            
+		    		}
+
 					$attributeOption[] = array(
 						'type'	=> 'attribute',
 						'label' => $option->getValue(),
 						'value' => $_value,
 						'count' => $option->getMatchCount(),
 						'selected' => $option->isSelected()
-					);	    				
-    				break;    				
+					);
+    				break;
     		}
 		}
-		return $attributeOption;    	
+		return $attributeOption;
     }
-    
+
     /**
      * get current FACT-Finder Catgory Path
-     * 
-     * @return string 
+     *
+     * @return string
      */
     protected function _getCurrentFactfinderCategoryPath($all = false)
     {
@@ -458,10 +458,10 @@ class Flagbit_FactFinder_Model_Adapter
         if(Mage::getStoreConfigFlag('factfinder/config/navigation') && Mage::registry('current_category')){
             /* @var $category Mage_Catalog_Model_Category */
             $category = Mage::registry('current_category');
-            
+
             $pathInStore = $category->getPathInStore();
             $pathIds = array_reverse(explode(',', $pathInStore));
-    
+
             $categories = $category->getParentCategories();
             $mainCategoriesString = '';
             foreach ($pathIds as $categoryId) {
@@ -473,19 +473,24 @@ class Flagbit_FactFinder_Model_Adapter
                     }
                     $mainCategoriesString .= '/'.$categories[$categoryId]->getName();
                 }
-            }                           
+            }
         }
         if($all === false){
-            $returnValue = $this->_currentFactfinderCategoryPath[count($this->_currentFactfinderCategoryPath)-1];
+        	if (isset($this->_currentFactfinderCategoryPath[count($this->_currentFactfinderCategoryPath)-1])) {
+            	$returnValue = $this->_currentFactfinderCategoryPath[count($this->_currentFactfinderCategoryPath)-1];
+        	}
+        	else {
+        		$returnValue = false;
+        	}
         }else{
             $returnValue = $this->_currentFactfinderCategoryPath;
         }
-        return $returnValue;        
+        return $returnValue;
     }
-    
+
     /**
      * get Attribute option Value
-     * 
+     *
      * @param string $option
      * @return string
      */
@@ -494,24 +499,24 @@ class Flagbit_FactFinder_Model_Adapter
     	$selectOptions = $this->_getSearchAdapter()->getSearchParams()->getFilters();
     	$value = null;
     	switch ($option->getType()) {
-    		
+
     		// handle Slider Attributes
     		case "slider";
 				$value = $option->getField().'|'.$option->getType().'|'.str_replace(array('&', '='), array('|', ':'), $option->getValue()).'[VALUE]';
     			break;
-    		
+
     		// handle default Attributes
     		default:
     			$value = $option->getField();
 				if($option->isSelected()){
 
-	    			// handle multiselectable Attributes				
+	    			// handle multiselectable Attributes
 	    			if(!empty($selectOptions[$option->getField()]) ){
 	    				if(strpos($option->getField(), 'categoryROOT') === false){
 	    					$values = explode('~~~', $selectOptions[$option->getField()]);
 	    					unset($values[array_search($option->getValue(), $values)]);
 	    					$value .= '|'.implode('~~~', $values);
-	    					
+
 	    				}else{
 	    					$values = explode('/',str_replace('|'.$selectOptions[$option->getField()], '', $value));
 	    					$valueCount = count($values);
@@ -522,28 +527,28 @@ class Flagbit_FactFinder_Model_Adapter
 	    						}
 	    					}
 	    				}
-	    			}					
+	    			}
 				}else{
 	    			$value .= '|'.$option->getValue();
-	    			// handle multiselectable Attributes				
+	    			// handle multiselectable Attributes
 	    			if(!empty($selectOptions[$option->getField()])){
 	    				$value .= '~~~'.$selectOptions[$option->getField()];
 	    			}
-				}	
+				}
     			break;
     	}
   		return $value;
     }
-    
-    
+
+
     /**
      * get Search Result Product Ids and additional Data
-     * 
+     *
      * @return array Products Ids
      */
     public function getSearchResultProductIds()
-    { 	
-    	if($this->_searchResultProductIds == null){ 	
+    {
+    	if($this->_searchResultProductIds == null){
 	    	$result = $this->_getSearchAdapter()->getResult();
 	    	$this->_searchResultProductIds = array();
 	    	if($result instanceof FACTFinder_Result){
@@ -555,16 +560,16 @@ class Flagbit_FactFinder_Model_Adapter
 						array(
 							'similarity' => $record->getSimilarity(),
 							'position' => $record->getPosition(),
-							'original_position' => $record->getOriginalPosition()																	
+							'original_position' => $record->getOriginalPosition()
 						)
 					);
 				}
 	    	}
-    	}    	
+    	}
 
     	return $this->_searchResultProductIds;
     }
-    
+
     /**
      * set single parameter, which will be looped through to the FACT-Finder request
      *
@@ -579,10 +584,10 @@ class Flagbit_FactFinder_Model_Adapter
         $this->_getDataProvider()->setParam($name, $value);
         return $this;
     }
-    
+
     /**
      * get FactFinder DataProvider
-     * 
+     *
      * @return FACTFinder_Abstract_DataProvider
      */
     protected function _getDataProvider()
@@ -594,22 +599,22 @@ class Flagbit_FactFinder_Model_Adapter
         }
         return $this->_dataProvider;
     }
-    
+
     /**
      * get Autentivation URL
-     * 
+     *
      * @return string
      */
     public function getAuthenticationUrl()
-    {	
+    {
     	$dataprovider = $this->_getDataProvider();
     	$dataprovider->setType('Management.ff');
-    	return $dataprovider->getAuthenticationUrl();   	
+    	return $dataprovider->getAuthenticationUrl();
     }
-    
+
     /**
      * get FactFinder Params Parser
-     * 
+     *
      * @return FACTFinder_ParametersParser
      */
     protected function _getParamsParser()
@@ -621,24 +626,24 @@ class Flagbit_FactFinder_Model_Adapter
         }
         return $this->_paramsParser;
     }
-    
+
     /**
      * set FactFinder Configuration
-     * 
+     *
      * @param array $configarray
      */
     public function setConfiguration($configarray)
     {
     	$this->_config = FF::getSingleton('configuration', $configarray);
     }
-    
+
     /**
      * get FactFinder Configuration
-     * 
+     *
      * @return FACTFinder_Abstract_Configuration config
      */
     protected function _getConfiguration($configarray = null)
-    {	
+    {
         if ($this->_config == null) {
             $this->_config = FF::getSingleton('configuration', $configarray);
         }
