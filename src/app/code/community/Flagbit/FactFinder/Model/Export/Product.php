1b510446fbd4dcf2f504f8d8a5c36f626811381b
rudibatt <rb@omikron.net>
2012-06-04 08:52:26 +0200

    [+BUGFIX] pass needed storeId var to getSearchableAttribtues method

--- src/app/code/community/Flagbit/FactFinder/Model/Export/Product.php
+++ src/app/code/community/Flagbit/FactFinder/Model/Export/Product.php
@@ -115,7 +115,7 @@ class Flagbit_FactFinder_Model_Export_Product extends Mage_CatalogSearch_Model_M
             }
             
             // get dynamic Attributes
-            foreach ($this->_getSearchableAttributes(null, 'system') as $attribute) {
+            foreach ($this->_getSearchableAttributes(null, 'system', $storeId) as $attribute) {
                 if (in_array($attribute->getAttributeCode(), array('sku', 'status', 'visibility'))) {
                     continue;
                 }            
@@ -158,7 +158,7 @@ class Flagbit_FactFinder_Model_Export_Product extends Mage_CatalogSearch_Model_M
         
         // preparesearchable attributes
         $staticFields   = array();
-        foreach ($this->_getSearchableAttributes('static', 'system') as $attribute) {
+        foreach ($this->_getSearchableAttributes('static', 'system', $storeId) as $attribute) {
             $staticFields[] = $attribute->getAttributeCode();
         }
         $dynamicFields  = array(
@@ -196,7 +196,6 @@ class Flagbit_FactFinder_Model_Export_Product extends Mage_CatalogSearch_Model_M
                 }
             }
 
-            $productIndexes     = array();
             $productAttributes  = $this->_getProductAttributes($storeId, $productAttributes, $dynamicFields);
             foreach ($products as $productData) {
                 if (!isset($productAttributes[$productData['entity_id']])) {
@@ -307,7 +306,7 @@ class Flagbit_FactFinder_Model_Export_Product extends Mage_CatalogSearch_Model_M
      * @param string $type possible Types: system, sortable, filterable, searchable
      * @return array
      */
-    protected function _getSearchableAttributes($backendType = null, $type = null)
+    protected function _getSearchableAttributes($backendType = null, $type = null, $storeId = null)
     {
         if (is_null($this->_searchableAttributes)) {
             $this->_searchableAttributes = array();
