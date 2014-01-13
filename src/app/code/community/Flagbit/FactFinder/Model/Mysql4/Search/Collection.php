6c2fdffdcd6961b92a575db3f92ae0d6eb0687d6
rudibatt <rb@omikron.net>
2014-01-13 10:20:52 +0100

    FIX: merge bugfix #54 into current development

--- src/app/code/community/Flagbit/FactFinder/Model/Mysql4/Search/Collection.php
+++ src/app/code/community/Flagbit/FactFinder/Model/Mysql4/Search/Collection.php
@@ -52,8 +52,8 @@ class Flagbit_FactFinder_Model_Mysql4_Search_Collection
 		// get product IDs from Fact-Finder
     	$productIds = $this->_getSearchHandler()->getSearchResult();
 
-        if($refKey = Mage::getSingleton('factfinder/facade')->getSearchResult()->getRefKey())
-        {
+        $searchResult = Mage::getSingleton('factfinder/facade')->getSearchResult();
+        if($searchResult && $refKey = $searchResult->getRefKey()) {
             Mage::getSingleton('core/session')->setFactFinderRefKey($refKey);
         }
 			
