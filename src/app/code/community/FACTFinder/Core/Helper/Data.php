1049510761d09b5f3650cc5167fa70c4585be2fa
xpoback <xpoback@gmail.com>
2015-03-31 16:48:17 +0200

    [FACTFINDER-78] Add module skeleton created by Magicento

---
+++ src/app/code/community/FACTFinder/Core/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+
+class FACTFinder_Core_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ src/app/code/community/FACTFinder/Core/sql/factfinder_core_setup/install-0.1.0.php
@@ -0,0 +1,10 @@
+<?php
+
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
