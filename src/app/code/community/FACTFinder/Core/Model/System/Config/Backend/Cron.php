5e35077239c4aef6ffb62c3236f0c0d9272e3567
xpoback <xpoback@gmail.com>
2015-04-15 21:39:31 +0200

    [FACTFINDER-78] Add cron export

---
+++ src/app/code/community/FACTFinder/Core/Model/System/Config/Backend/Cron.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Log Cron Backend Model
+ */
+class FACTFinder_Core_Model_System_Config_Backend_Cron extends Mage_Core_Model_Config_Data
+{
+    const CRON_STRING_PATH  = 'crontab/jobs/factfinder_generate/schedule/cron_expr';
+    const CRON_MODEL_PATH   = 'crontab/jobs/factfinder_generate/run/model';
+
+    /**
+     * Cron settings after save
+     *
+     * @return Mage_Adminhtml_Model_System_Config_Backend_Log_Cron
+     */
+    protected function _afterSave()
+    {
+        $enabled    = $this->getData('groups/cron/fields/enabled/value');
+        $time       = $this->getData('groups/cron/fields/time/value');
+        $frequncy   = $this->getData('groups/cron/fields/frequency/value');
+
+        $frequencyDaily     = Mage_Adminhtml_Model_System_Config_Source_Cron_Frequency::CRON_DAILY;
+        $frequencyWeekly    = Mage_Adminhtml_Model_System_Config_Source_Cron_Frequency::CRON_WEEKLY;
+        $frequencyMonthly   = Mage_Adminhtml_Model_System_Config_Source_Cron_Frequency::CRON_MONTHLY;
+
+        if ($enabled) {
+            $cronExprArray = array(
+                intval($time[1]),                                   # Minute
+                intval($time[0]),                                   # Hour
+                ($frequncy == $frequencyMonthly) ? '1' : '*',       # Day of the Month
+                '*',                                                # Month of the Year
+                ($frequncy == $frequencyWeekly) ? '1' : '*',        # Day of the Week
+            );
+            $cronExprString = join(' ', $cronExprArray);
+        } else {
+            $cronExprString = '';
+        }
+
+        try {
+            Mage::getModel('core/config_data')
+                ->load(self::CRON_STRING_PATH, 'path')
+                ->setValue($cronExprString)
+                ->setPath(self::CRON_STRING_PATH)
+                ->save();
+
+            Mage::getModel('core/config_data')
+                ->load(self::CRON_MODEL_PATH, 'path')
+                ->setValue((string) Mage::getConfig()->getNode(self::CRON_MODEL_PATH))
+                ->setPath(self::CRON_MODEL_PATH)
+                ->save();
+        } catch (Exception $e) {
+            Mage::throwException(Mage::helper('adminhtml')->__('Unable to save the cron expression.'));
+        }
+    }
+}
