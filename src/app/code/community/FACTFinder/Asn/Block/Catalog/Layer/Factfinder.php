11e57a50602fb880277a626dcafe98e41baefb1d
xpoback <xpoback@gmail.com>
2015-04-17 13:03:20 +0200

    [FACTFINDER-80] Add slider filer type

--- src/app/code/community/FACTFinder/Asn/Block/Catalog/Layer/Factfinder.php
+++ src/app/code/community/FACTFinder/Asn/Block/Catalog/Layer/Factfinder.php
@@ -2,9 +2,25 @@
 
 class FACTFinder_Asn_Block_Catalog_Layer_Factfinder extends Mage_Catalog_Block_Layer_Filter_Attribute
 {
+    const TYPE_SLIDER = 'slider';
+
     public function __construct()
     {
         parent::__construct();
         $this->_filterModelName = 'factfinder_asn/layer_filter_factfinder';
+    }
+
+    public function init()
+    {
+        parent::init();
+
+        $attribute = $this->getAttributeModel();
+        if ($attribute->getType() == self::TYPE_SLIDER) {
+            $this->setTemplate('factfinder/asn/layer/filter/slider.phtml');
+            $this->setData((current($attribute->getItems())));
+            $this->setUnit($attribute->getUnit());
+        }
+
+        return $this;
     }
 }
