deba8151ec61c110fe1e2f57cce30687833df802
xpoback <xpoback@gmail.com>
2015-04-17 21:07:21 +0200

    [FACTFINDER-80] Add replacing layered navigation in catalog

--- src/app/code/community/FACTFinder/Asn/Model/Observer.php
+++ src/app/code/community/FACTFinder/Asn/Model/Observer.php
@@ -3,6 +3,35 @@
 class FACTFinder_Asn_Model_Observer
 {
 
+
+    public function addLayerFiltersToCatalog($observer)
+    {
+        $block = $observer->getBlock();
+
+        if (!$block instanceof Mage_Catalog_Block_Layer_View) {
+            return;
+        }
+
+        $stateBlock = $block->getLayout()->createBlock('catalog/layer_state')
+            ->setLayer($block->getLayer());
+
+        $block->setChild('layer_state', $stateBlock);
+
+        $filterableAttributes = Mage::getResourceModel('factfinder_asn/product_attribute_collection');
+        foreach ($filterableAttributes as $attribute) {
+            $filter = $block->getLayout()
+                ->createBlock('factfinder_asn/catalog_layer_factfinder')
+                ->setAttributeModel($attribute)
+                ->setLayer($block->getLayer())
+                ->init();
+
+            $block->setChild($attribute->getAttributeCode() . '_filter', $filter);
+        }
+
+        $block->setData('_filterable_attributes', $filterableAttributes);
+    }
+
+
     /**
      * Add factfinder filter block type
      *
