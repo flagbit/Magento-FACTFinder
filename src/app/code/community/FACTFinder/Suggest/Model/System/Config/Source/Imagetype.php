235e2d3e22cf393c3cc3e2b50acd954fad5c5b94
xpoback <xpoback@gmail.com>
2015-05-07 14:58:54 +0200

    [+FIX] Add a missing file

---
+++ src/app/code/community/FACTFinder/Suggest/Model/System/Config/Source/Imagetype.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Image type source model
+ */
+class FACTFinder_Suggest_Model_System_Config_Source_Imagetype
+{
+    /**
+     * Get available image types
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $options = array();
+        foreach (Mage::getModel('catalog/product')->getMediaAttributes() as $key => $value) {
+            $options[] = array(
+                'label' => $value->getFrontendLabel(),
+                'value' => $key
+            );
+        }
+
+        return $options;
+    }
+}
