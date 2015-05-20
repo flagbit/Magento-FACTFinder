c932dd6b0367357d519d397901f50e93251cbb01
thakilla <essig@flagbit.de>
2015-05-20 20:55:35 +0200

    [FACTFINDER-84] migrated frontend tracking stuff

---
+++ src/app/code/community/FACTFinder/Tracking/Block/Init.php
@@ -0,0 +1,72 @@
+<?php
+class FACTFinder_Tracking_Block_Init extends Mage_Core_Block_Template
+{
+    /**
+     * get Product Result Collection
+     *
+     * @return Flagbit_FactFinder_Model_Mysql4_Search_Collection
+     */
+    protected function _getProductResultCollection()
+    {
+        return Mage::getSingleton('factfinder/catalogSearch_layer')->getProductCollection();
+    }
+
+    /**
+     * get Product URL to ID Mapping JSON Object
+     *
+     * @return string
+     */
+    public function getJsonUrlToIdMappingObject()
+    {
+        $data = array();
+        foreach($this->_getProductResultCollection() as $product){
+            $data[$product->getProductUrl()] = $product->getId();
+        }
+        return Mage::helper('core')->jsonEncode($data);
+    }
+
+    /**
+     * get Product and Search Details by ID as JSON Object
+     *
+     * @return string
+     */
+    public function getJsonDataObject()
+    {
+        $searchHelper = Mage::helper('factfinder/search');
+        $idFieldName = $searchHelper->getIdFieldName();
+
+        $customerId = Mage::getSingleton('customer/session')->getCustomer()->getId();
+        if ($customerId) {
+            $customerId = md5('customer_' . $customerId);
+        }
+
+        $dataTemplate = array(
+            'query'         => $searchHelper->getQuery()->getQueryText(),
+            'page'          => $searchHelper->getCurrentPage(),
+            'sid'           => md5(Mage::getSingleton('core/session')->getSessionId()),
+            'pageSize'      => $searchHelper->getPageLimit(),
+            'origPageSize'  => $searchHelper->getDefaultPerPageValue(),
+            'channel'       => Mage::getStoreConfig('factfinder/search/channel'),
+            'userId'        => $customerId,
+            'event'         => 'click'
+        );
+
+        $data = array();
+        foreach($this->_getProductResultCollection() as $product){
+            $key = $product->getId();
+
+            $data[$key] = array(
+                'id' => $product->getData($idFieldName),
+                'masterid' => $product->getData($idFieldName),
+                'pos'		=> $product->getPosition(),
+                'origPos'	=> $product->getOriginalPosition() ? $product->getOriginalPosition() : $product->getPosition(),
+                'title'		=> $product->getName(),
+                'simi'		=> $product->getSimilarity()
+            );
+
+            $data[$key] += $dataTemplate;
+        }
+
+        return Mage::helper('core')->jsonEncode($data);
+    }
+}
--- src/app/code/community/FACTFinder/Tracking/Helper/Data.php
+++ src/app/code/community/FACTFinder/Tracking/Helper/Data.php
@@ -15,27 +15,15 @@ class FACTFinder_Tracking_Helper_Data extends Mage_Core_Helper_Abstract
     }
 
     /**
-     * Decide whether legacy tracking should be used (old tracking for the versions for 6.8 and 6.9)
-     *
-     * @return bool
-     */
-    public function useLegacyTracking()
-    {
-        $ffVersion = Mage::getStoreConfig('factfinder/search/ffversion');
-        return ($ffVersion >= 68 && $ffVersion <= 69);
-    }
-
-    /**
      * returns the correct path where the tracking should be sent
      *
      * @return string
      */
     public function getTrackingUrlPath()
     {
-        $urlPath = 'factfinder/proxy/tracking';
-        if ($this->useOldTracking() && !$this->useLegacyTracking()) {
-            // if old tracking is legacy tracking, don't use the scic url
-            $urlPath = 'factfinder/proxy/scic';
+        $urlPath = 'ff_tracking/proxy/tracking';
+        if ($this->useOldTracking()) {
+            $urlPath = 'ff_tracking/proxy/scic';
         }
         return $urlPath;
     }
