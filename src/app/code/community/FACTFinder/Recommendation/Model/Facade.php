eadf4f950458c80f8be7c55ce6074cc20845a3e2
xpoback <xpoback@gmail.com>
2015-07-02 17:24:45 +0200

    [+TASK] Add phpdoc for files and classes in recommendations

--- src/app/code/community/FACTFinder/Recommendation/Model/Facade.php
+++ src/app/code/community/FACTFinder/Recommendation/Model/Facade.php
@@ -1,4 +1,28 @@
 <?php
+/**
+ * FACTFinder_Recommendation
+ *
+ * @category Mage
+ * @package FACTFinder_Recommendation
+ * @author Flagbit Magento Team <magento@flagbit.de>
+ * @copyright Copyright (c) 2015, Flagbit GmbH & Co. KG
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link http://www.flagbit.de
+ *
+ */
+
+/**
+ * Class FACTFinder_Recommendation_Model_Facade
+ *
+ * Facade class that adds recommendations functionality to the core facade
+ *
+ * @category Mage
+ * @package FACTFinder_Recommendation
+ * @author Flagbit Magento Team <magento@flagbit.de>
+ * @copyright Copyright (c) 2015, Flagbit GmbH & Co. KG
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link http://www.flagbit.de
+ */
 class FACTFinder_Recommendation_Model_Facade extends FACTFinder_Core_Model_Facade
 {
 
--- src/app/code/community/FACTFinder/Recommendation/Model/Handler/Recommendations.php
+++ src/app/code/community/FACTFinder/Recommendation/Model/Handler/Recommendations.php
@@ -1,7 +1,27 @@
 <?php
+/**
+ * FACTFinder_Recommendation
+ *
+ * @category Mage
+ * @package FACTFinder_Recommendation
+ * @author Flagbit Magento Team <magento@flagbit.de>
+ * @copyright Copyright (c) 2015, Flagbit GmbH & Co. KG
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link http://www.flagbit.de
+ *
+ */
 
 /**
+ * Class FACTFinder_Recommendation_Model_Handler_Recommendations
+ *
  * Handles product recommendation data
+ *
+ * @category Mage
+ * @package FACTFinder_Recommendation
+ * @author Flagbit Magento Team <magento@flagbit.de>
+ * @copyright Copyright (c) 2015, Flagbit GmbH & Co. KG
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link http://www.flagbit.de
  */
 class FACTFinder_Recommendation_Model_Handler_Recommendations extends FACTFinder_Core_Model_Handler_Abstract
 {
