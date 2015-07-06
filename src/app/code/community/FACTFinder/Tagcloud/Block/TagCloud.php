ce329a5221d785de555ffadc44c6feaf4247ec0e
xpoback <xpoback@gmail.com>
2015-07-06 13:54:07 +0200

    [+TASK] Add phpdoc file blocks to TagCloud

--- src/app/code/community/FACTFinder/Tagcloud/Block/TagCloud.php
+++ src/app/code/community/FACTFinder/Tagcloud/Block/TagCloud.php
@@ -1,4 +1,28 @@
 <?php
+/**
+ * FACTFinder_TagCloud
+ *
+ * @category Mage
+ * @package FACTFinder_TagCloud
+ * @author Flagbit Magento Team <magento@flagbit.de>
+ * @copyright Copyright (c) 2015 Flagbit GmbH & Co. KG
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link http://www.flagbit.de
+ *
+ */
+
+/**
+ * Block class
+ *
+ * Is used instead of the default block
+ *
+ * @category Mage
+ * @package FACTFinder_TagCloud
+ * @author Flagbit Magento Team <magento@flagbit.de>
+ * @copyright Copyright (c) 2015 Flagbit GmbH & Co. KG
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link http://www.flagbit.de
+ */
 class FACTFinder_Tagcloud_Block_TagCloud extends Mage_CatalogSearch_Block_Term
 {
 
--- src/app/code/community/FACTFinder/Tagcloud/Model/Facade.php
+++ src/app/code/community/FACTFinder/Tagcloud/Model/Facade.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * FACTFinder_TagCloud
+ *
+ * @category Mage
+ * @package FACTFinder_TagCloud
+ * @author Flagbit Magento Team <magento@flagbit.de>
+ * @copyright Copyright (c) 2015 Flagbit GmbH & Co. KG
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link http://www.flagbit.de
+ *
+ */
+
+/**
+ * Model class
+ *
+ * @category Mage
+ * @package FACTFinder_TagCloud
+ * @author Flagbit Magento Team <magento@flagbit.de>
+ * @copyright Copyright (c) 2015 Flagbit GmbH & Co. KG
+ * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link http://www.flagbit.de
+ */
 class FACTFinder_Tagcloud_Model_Facade extends FACTFinder_Core_Model_Facade
 {
 
