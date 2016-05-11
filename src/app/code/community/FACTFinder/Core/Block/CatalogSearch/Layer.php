00813e0b3a85caf5339176bedf56c18ecccf8ce3
xpoback <xpoback@gmail.com>
2016-05-11 16:37:11 +0200

    [FACTFINDER-174] Prevent writing queries to the db when FF is enabled

---
+++ src/app/code/community/FACTFinder/Core/Block/CatalogSearch/Layer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * View.php
+ *
+ * @category Mage
+ * @package FACTFinder_Core
+ * @author Flagbit Magento Team <magento@flagbit.de>
+ * @copyright Copyright (c) 2016 Flagbit GmbH & Co. KG
+ * @license GPL
+ * @link http://www.flagbit.de
+ */
+class FACTFinder_Core_Block_CatalogSearch_Layer extends Mage_CatalogSearch_Block_Layer
+{
+
+
+    /**
+     * Rewrite the block to prevent using default layer model
+     *
+     * @return FACTFinder_Core_Model_CatalogSearch_Layer|Mage_Catalog_Model_Layer
+     */
+    public function getLayer()
+    {
+        if (Mage::helper('factfinder')->isEnabled()) {
+            return Mage::getSingleton('factfinder/catalogSearch_layer');
+        }
+
+        return parent::getLayer();
+    }
+
+
+}
---
+++ src/app/code/community/FACTFinder/Core/Model/CatalogSearch/Query.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Query.php
+ *
+ * @category Mage
+ * @package FACTFinder_Core
+ * @author Flagbit Magento Team <magento@flagbit.de>
+ * @copyright Copyright (c) 2016 Flagbit GmbH & Co. KG
+ * @license GPL
+ * @link http://www.flagbit.de
+ */
+
+class FACTFinder_Core_Model_CatalogSearch_Query extends Mage_CatalogSearch_Model_Query
+{
+
+
+    /**
+     * Rewrite the model to prevent savin data to the db if FF is enabled
+     *
+     * @return $this|\Mage_Core_Model_Abstract
+     *
+     * @throws \Exception
+     */
+    public function save()
+    {
+        if (Mage::helper('factfinder')->isEnabled()) {
+            return $this;
+        }
+
+        return parent::save();
+    }
+
+
+}
