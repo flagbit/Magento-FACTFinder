cc4c96fb9bc90b90628381ff6e35d073f2d78b1f
xpoback <xpoback@gmail.com>
2017-02-20 14:25:27 +0100

    [FACTFINDER-163] Refactor attribute export

---
+++ src/app/code/community/FACTFinder/Core/Model/Export/Type/Product/Attribute.php
@@ -0,0 +1,509 @@
+<?php
+
+/**
+ * Class FACTFinder_Core_Model_Export_Type_Product_Attribute
+ */
+class FACTFinder_Core_Model_Export_Type_Product_Attribute extends Mage_Core_Model_Abstract
+{
+
+    /**
+     * Option ID to Value Mapping Array
+     *
+     * @var mixed
+     */
+    protected $_optionIdToValue;
+
+    /**
+     * @var array
+     */
+    protected $_exportAttributeCodes;
+
+    /**
+     * @var
+     */
+    protected $_engine;
+
+    /**
+     * @var array
+     */
+    protected $_exportAttributes;
+
+
+    protected function _construct()
+    {
+        parent::_construct();
+
+        $this->_init('factfinder/attribute');
+        $this->_engine = Mage::helper('catalogsearch')->getEngine();
+    }
+
+
+    /**
+     * @param $optionId
+     * @param $storeId
+     *
+     * @return mixed|string
+     */
+    public function getOptionText($optionId, $storeId)
+    {
+        $value = '';
+        if (intval($optionId)) {
+            if ($this->_optionIdToValue === null) {
+                /** @var Mage_Eav_Model_Resource_Entity_Attribute_Option_Collection $optionCollection */
+                $optionCollection = Mage::getResourceModel('eav/entity_attribute_option_collection');
+                $optionCollection->setStoreFilter($storeId);
+                $this->_optionIdToValue = array();
+                foreach ($optionCollection as $option) {
+                    $this->_optionIdToValue[$option->getId()] = $option->getValue();
+                }
+            }
+
+            $value = isset($this->_optionIdToValue[$optionId]) ? $this->_optionIdToValue[$optionId] : '';
+        }
+
+        return $value;
+    }
+
+
+    /**
+     * Retrieve attribute source value for search
+     * This method is mostly copied from Mage_CatalogSearch_Model_Resource_Fulltext,
+     * but it also retrieves attribute values from non-searchable/non-filterable attributes
+     *
+     * @param int   $attributeId
+     * @param mixed $value
+     * @param int   $storeId
+     *
+     * @return mixed
+     */
+    public function getAttributeValue($attributeId, $value, $storeId)
+    {
+        $attribute = $this->getResource()->getSearchableAttribute($attributeId);
+        if (!$attribute->getIsSearchable() && $attribute->getAttributeCode() == 'visibility') {
+            return $value;
+        }
+
+        if ($attribute->usesSource()) {
+            if (method_exists($this->_engine, 'allowAdvancedIndex') && $this->_engine->allowAdvancedIndex()) {
+                return $value;
+            }
+
+            $attribute->setStoreId($storeId);
+            $value = $attribute->getSource()->getOptionText($value);
+
+            if (is_array($value)) {
+                $value = implode('|', $value);
+            } elseif (empty($value)) {
+                $inputType = $attribute->getFrontend()->getInputType();
+                if ($inputType == 'select' || $inputType == 'multiselect') {
+                    return null;
+                }
+            }
+        } elseif ($attribute->getBackendType() == 'datetime') {
+            $value = strtotime($value) * 1000; // Java.lang.System.currentTimeMillis()
+        } else {
+            $inputType = $attribute->getFrontend()->getInputType();
+            if ($inputType == 'price') {
+                $value = Mage::app()->getStore($storeId)->roundPrice($value);
+            }
+        }
+
+        return $value;
+    }
+
+
+    /**
+     * @param null $storeId
+     *
+     * @return mixed
+     */
+    public function getSearchableAttributes($storeId = null)
+    {
+        return $this->getResource()->getSearchableAttributes($storeId);
+    }
+
+    /**
+     * Retrieve searchable attribute by Id or code
+     *
+     * @param int|string $attribute
+     *
+     * @return Mage_Eav_Model_Entity_Attribute
+     */
+    public function getSearchableAttribute($attribute)
+    {
+        return $this->getResource()->getSearchableAttribute($attribute);
+    }
+
+    /**
+     * Load product(s) attributes
+     *
+     * @param int   $storeId
+     * @param array $productIds
+     * @param array $attributeTypes
+     *
+     * @return array
+     */
+    public function getProductAttributes($storeId, array $productIds, array $attributeTypes)
+    {
+        return $this->getResource()->getProductAttributes($storeId, $productIds, $attributeTypes);
+    }
+
+
+    /**
+     * Get CSV Header Array
+     *
+     * @param int $storeId
+     *
+     * @return array
+     */
+    public function getExportAttributes($storeId = 0)
+    {
+        if (!isset($this->_exportAttributeCodes[$storeId])) {
+            $headerDynamic = array();
+
+            // get dynamic Attributes
+            foreach ($this->getSearchableAttributesByType(null, 'system', $storeId) as $attribute) {
+                if (in_array($attribute->getAttributeCode(), array('sku', 'status', 'visibility'))) {
+                    continue;
+                }
+
+                $headerDynamic[] = $attribute->getAttributeCode();
+            }
+
+
+            $this->_exportAttributeCodes[$storeId] = $headerDynamic;
+        }
+
+        return $this->_exportAttributeCodes[$storeId];
+    }
+
+    /**
+     * Get searchable attributes by type
+     *
+     * @param null   $backendType Backend type of the attributes
+     * @param string $type        Possible Types: system, sortable, filterable, searchable
+     * @param int    $storeId
+     *
+     * @return array
+     */
+    public function getSearchableAttributesByType($backendType = null, $type = null, $storeId = 0)
+    {
+        $attributes = array();
+
+        if ($type !== null || $backendType !== null) {
+            foreach ($this->getSearchableAttributes($storeId) as $attribute) {
+                if ($backendType !== null
+                    && $attribute->getBackendType() != $backendType
+                ) {
+                    continue;
+                }
+
+                if ($this->_checkIfSkipAttribute($attribute, $type)) {
+                    continue;
+                }
+
+                $attributes[$attribute->getId()] = $attribute;
+            }
+        } else {
+            $attributes = $this->getSearchableAttributes($storeId);
+        }
+
+        return $attributes;
+    }
+
+    /**
+     * Check whether the attribute should be skipped
+     *
+     * @param Mage_Catalog_Model_Resource_EAV_Attribute $attribute
+     * @param string                                    $type
+     *
+     * @return bool
+     */
+    protected function _checkIfSkipAttribute($attribute, $type)
+    {
+        $shouldSkip = false;
+        switch ($type) {
+            case "system":
+                if ($attribute->getIsUserDefined() && !$attribute->getUsedForSortBy()) {
+                    $shouldSkip = true;
+                }
+                break;
+            case "sortable":
+                if (!$attribute->getUsedForSortBy()) {
+                    $shouldSkip = true;
+                }
+                break;
+            case "filterable":
+                $shouldSkip = $this->_shouldSkipFilterableAttribute($attribute);
+                break;
+            case 'numerical':
+                $shouldSkip = $this->_shouldSkipNumericalAttribute($attribute);
+                break;
+            case "searchable":
+                $shouldSkip = $this->_shouldSkipSearchableAttribute($attribute);
+                break;
+            default:;
+        }
+
+        return $shouldSkip;
+    }
+
+    /**
+     * Check if we should skip searchable attribute
+     *
+     * @param Mage_Catalog_Model_Resource_EAV_Attribute $attribute
+     *
+     * @return bool
+     */
+    protected function _shouldSkipSearchableAttribute($attribute)
+    {
+        if (!$attribute->getIsUserDefined()
+            || !$attribute->getIsSearchable()
+            || in_array($attribute->getAttributeCode(), $this->getExportAttributes())
+            || $attribute->getBackendType() === 'decimal'
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Check if we should skip filterable attribute
+     *
+     * @param Mage_Catalog_Model_Resource_EAV_Attribute $attribute
+     *
+     * @return bool
+     */
+    protected function _shouldSkipFilterableAttribute($attribute)
+    {
+        if (!$attribute->getIsFilterableInSearch()
+            || in_array($attribute->getAttributeCode(), $this->getExportAttributes())
+            || $attribute->getBackendType() === 'decimal'
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Check if we should skip numerical attribute
+     *
+     * @param Mage_Catalog_Model_Resource_EAV_Attribute $attribute
+     *
+     * @return bool
+     */
+    protected function _shouldSkipNumericalAttribute($attribute)
+    {
+        if (!$attribute->getIsFilterableInSearch()
+            || in_array($attribute->getAttributeCode(), $this->getExportAttributes())
+            || $attribute->getBackendType() != 'decimal'
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Get array of static product fields
+     *
+     * @param int $storeId
+     *
+     * @return array
+     */
+    public function getStaticFields($storeId)
+    {
+        $staticFields = array();
+        foreach ($this->getSearchableAttributesByType('static', 'system', $storeId) as $attribute) {
+            $staticFields[] = $attribute->getAttributeCode();
+        }
+        return $staticFields;
+    }
+
+
+    /**
+     * Get array of dynamic fields to use in csv
+     *
+     * @param $storeId
+     *
+     * @return array
+     */
+    public function getDynamicFields($storeId)
+    {
+        $dynamicFields = array();
+        foreach (array('int', 'varchar', 'text', 'decimal', 'datetime') as $type) {
+            $dynamicFields[$type] = array_keys($this->getSearchableAttributesByType($type, null, $storeId));
+        }
+
+        return $dynamicFields;
+    }
+
+
+    /**
+     * Get Attribute Row Array
+     *
+     * @param array $dataArray Export row Array
+     * @param array $values    Attributes Array
+     * @param int   $storeId   Store ID
+     *
+     * @return array
+     */
+    public function addAttributesToRow($dataArray, $values, $storeId = null)
+    {
+        // get attributes objects assigned to their position at the export
+        if ($this->_exportAttributes == null) {
+            $attributes = $this->getExportAttributes($storeId);
+            $this->_exportAttributes = array_fill(
+                0,
+                count($attributes),
+                null
+            );
+
+            $attributeCodes = array_flip($attributes);
+            $searchableAttributes = $this->getSearchableAttributesByType();
+            foreach ($searchableAttributes as $attribute) {
+                if (isset($attributeCodes[$attribute->getAttributeCode()])
+                    && !in_array($attribute->getAttributeCode(), array('sku', 'status', 'visibility'))
+                ) {
+                    $this->_exportAttributes[$attributeCodes[$attribute->getAttributeCode()]] = $attribute;
+                }
+            }
+        }
+
+        // fill dataArray with the values of the attributes that should be exported
+        foreach ($this->_exportAttributes as $attribute) {
+            if ($attribute != null) {
+                $value = isset($values[$attribute->getId()]) ? $values[$attribute->getId()] : null;
+                $value = $this->getAttributeValue($attribute->getId(), $value, $storeId);
+                $value = $this->_removeTags($value, $storeId);
+                $dataArray[] = $value;
+            } else {
+                $dataArray[] = null;
+            }
+        }
+
+        return $dataArray;
+    }
+
+
+    /**
+     * Format attributes for csv
+     *
+     * @param string   $type
+     * @param array    $values
+     * @param null|int $storeId
+     *
+     * @return string
+     */
+    public function formatAttributes($type, $values, $storeId = null)
+    {
+        /**
+         * Get attributes by type
+         *
+         * @param string $type    Possible values: filterable|searchable|numerical
+         *
+         */
+        switch ($type) {
+            case 'numerical':
+                $attributes = $this->getSearchableAttributesByType('decimal', $type, $storeId);
+                break;
+            default:
+                $attributes = $this->getSearchableAttributesByType(null, $type, $storeId);
+        }
+
+        $returnArray = array();
+        $counter = 0;
+
+        foreach ($attributes as $attribute) {
+            $attributeValue = isset($values[$attribute->getId()]) ? $values[$attribute->getId()] : null;
+            if (!$attributeValue
+                || in_array($attribute->getAttributeCode(), array('sku', 'status', 'visibility', 'price'))
+            ) {
+                continue;
+            }
+
+            $attributeValues = $this->getAttributeValue($attribute->getId(), $attributeValue, $storeId);
+
+            if (!is_array($attributeValues)) {
+                $attributeValues = array($attributeValues);
+            }
+
+            $attributeValues = $this->_filterAttributeValues($attributeValues);
+            foreach ($attributeValues as $attributeValue) {
+                $attributeValue = $this->_removeTags($attributeValue, $storeId);
+                if ($type == 'searchable') {
+                    $returnArray[] = $attributeValue;
+                } else {
+                    $attributeCode = $this->_removeTags($attribute->getAttributeCode(), $storeId);
+                    $attributeValue = str_replace(array('|', '=', '#'), '', array($attributeCode, $attributeValue));
+                    $returnArray[] = implode('=', $attributeValue);
+                }
+            }
+
+            // apply field limit as required by ff
+            $counter++;
+            if ($counter >= 1000) {
+                break;
+            }
+        }
+
+        $delimiter = ($type == 'searchable' ? ',' : '|');
+
+        return implode($delimiter, $returnArray);
+    }
+
+
+    /**
+     * Check if html tags and entities should be removed on export
+     *
+     * @param string $value
+     * @param int    $storeId
+     *
+     * @return bool
+     */
+    protected function _removeTags($value, $storeId)
+    {
+        if (Mage::getStoreConfig('factfinder/export/remove_tags', $storeId)) {
+            $attributeValues = $value;
+            if (!is_array($attributeValues)) {
+                $attributeValues = array($value);
+            }
+            foreach ($attributeValues as &$attributeValue) {
+                // decode html entities
+                $attributeValue = html_entity_decode($attributeValue, null, 'UTF-8');
+                // Add spaces before HTML Tags, so that strip_tags() does not join word which were in different block elements
+                // Additional spaces are not an issue, because they will be removed in the next step anyway
+                $attributeValue = preg_replace('/</u', ' <', $attributeValue);
+                $attributeValue = preg_replace("#\s+#siu", ' ', trim(strip_tags($attributeValue)));
+                // remove rest html entities
+                $attributeValue = preg_replace("/&(?:[a-z\d]|#\d|#x[a-f\d]){2,8};/i", '', $attributeValue);
+            }
+            $value = implode("|", $attributeValues);
+        }
+
+        return $value;
+    }
+
+
+    /**
+     * Remove all empty values from array
+     *
+     * @param array $values
+     *
+     * @return array
+     */
+    protected function _filterAttributeValues($values)
+    {
+        // filter all empty values out
+        return array_filter($values, function ($value) {
+            return !empty($value);
+        });
+    }
+
+
+}
